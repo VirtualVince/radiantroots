dfa727fb263c841df0f5fd49f3364f3eef532b4f
VirtualVince <virtualvince2020@gmail.com>
2026-05-29 10:45:39 -0400

    favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -30,6 +30,14 @@ export const metadata: Metadata = {
     locale: "en_US",
     type: "website",
   },
+   icons: {
+    icon: [
+      { url: "/favicon_io/favicon-16x16.png", sizes: "16x16", type: "image/png" },
+      { url: "/favicon_io/favicon-32x32.png", sizes: "32x32", type: "image/png" },
+    ],
+    apple: { url: "/favicon_io/apple-touch-icon.png", sizes: "180x180", type: "image/png" },
+  },
+  manifest: "/favicon_io/site.webmanifest",
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
